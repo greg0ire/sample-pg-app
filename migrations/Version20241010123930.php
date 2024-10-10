b950468871b68aafc98e654a8ae57abf30731db7
Christopher Georg <christopher.georg@sr-travel.de>
2024-10-10 14:41:34 +0200

    feat: migration

---
+++ migrations/Version20241010123930.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ *
+ * symfony console doctrine:migrations:diff -n
+ * symfony console doctrine:migrations:migrate -n
+ */
+final class Version20241010123930 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pokemon (name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, level INT NOT NULL, PRIMARY KEY(name))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE pokemon');
+    }
+}
